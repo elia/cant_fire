95abaf3e46356e630b6d1d6f7d49f932f59e0061
Elia Schito <elia@schito.me>
2012-11-23 11:01:00 +0100

    Write to CF from your favorite $EDITOR

--- lib/cant_fire.rb
+++ lib/cant_fire.rb
@@ -3,17 +3,41 @@ require 'cant_fire/version'
 module CantFire
   extend self
 
+  def new_message &block
+    room    = $room || @rooms.first
+    message = $message
+
+    file = File.expand_path('~/.cant_fire_message')
+
+    File.open(file, 'w') do |m|
+      m.puts "# #{room.id}: #{room.name.inspect}"
+      m.puts "#   users: #{room.users.map(&:name)}"
+      m.puts "#{message.user.name}: " if message
+    end
+    puts [ENV['EDITOR'], '-l3:-1', file].join(' ')
+    system [ENV['EDITOR'], '-l3:-1', file].join(' ')
+    message = File.read(file).lines.select { |l| l !~ /^\s*#/ }.join("\n")
+    room.speak message unless message.strip.empty?
+    block.call
+  end
+
+
   def start!
+    require 'terminal-notifier'
     require 'term/ansicolor'
+    require 'thread'
+    require 'tinder'
+    require 'shellwords'
+
+
     String.send :include, Term::ANSIColor
 
     colors = %w[red green yellow blue magenta cyan white]
 
     room_colors = {all: colors.dup}
     user_colors = {all: colors.dup}
-
-    require 'thread'
-    @rooms = campfire.rooms.map do |room|
+    @rooms = campfire.rooms
+    @rooms_threads = @rooms.map do |room|
       next if config.skip_rooms.include? room.name
       Thread.new do
         room_colors[:all] = colors.dup if room_colors[:all].empty?
@@ -23,6 +47,9 @@ module CantFire
         puts "Connecting to #{room.name.inspect}".send(room_color)
         room.listen do |message|
           next unless message.body and message.user
+
+          $room = room
+          $message = message
 
           user = message.user
 
@@ -49,7 +76,22 @@ module CantFire
       end
     end.compact
 
-    @rooms.each(&:join)
+
+    begin
+      puts 'Hit CTRL+C to write a message'
+      sleep 1 until $exit
+    rescue Interrupt
+      if $interrupted
+        $exit = true
+      else
+        $interrupted = true
+        puts 'Hit CTRL+C to exit'
+        CantFire.new_message { $interrupted = false }
+        retry
+      end
+    end
+    @rooms_threads.each(&:exit)
+    exit
   end
 
 
@@ -67,6 +109,7 @@ module CantFire
     @config ||= begin
       require 'ostruct'
       require 'yaml'
+
       config_path = File.expand_path('~/.cant_fire')
       raise ConfigError.new("Please setup your config file first in: #{config_path}") unless File.exist? config_path
       OpenStruct.new YAML.load_file(config_path)
@@ -75,7 +118,6 @@ module CantFire
 
   def campfire
     @campfire ||= begin
-      require 'tinder'
       Tinder::Campfire.new config.subdomain, :token => config.token
     end
   end
@@ -85,9 +127,7 @@ module CantFire
   end
 
   def notify message, options
-    require 'shellwords'
     fork { exec "say #{options[:title].shellescape}" }
-    require 'terminal-notifier'
     # TerminalNotifier.notify('Hello World', :title => 'Ruby', :subtitle => 'Programming Language')
     # TerminalNotifier.notify('Hello World', :activate => 'com.apple.Safari')
     # TerminalNotifier.notify('Hello World', :open => 'http://twitter.com/alloy')
